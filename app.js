c2d4d6ba3f08fd78570e78b3c9b8e22b499d3976
G <giovannivajnadepava@gmail.com>
2014-03-22 18:27:23 +0100

    first round

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var http=require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {
-        'Content-Type':'application/json',
-        'Access-Control-Allow-Origin':'*'
+        'Content-Type':'application/json'
     });
     res.end('{"Hello":"World"}');
-}).listen(8080, '127.0.0.1');
+}).listen(3000, '127.0.0.1');
 console.log('Running!');
